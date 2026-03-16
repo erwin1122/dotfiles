3cd6042608e76bb8b0bb6493b1325ebd5ca56195
Erwin Naibauer <erwin1997@ymail.com>
2026-03-16 11:18:46 +0100

    Attach to tmux session when existing

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,8 +5,10 @@ if not status is-interactive
     return
 end
 
-# Show system info on shell start
-fastfetch
+# Show system info on shell start (only if installed)
+if type -q fastfetch
+    fastfetch
+end
 
 # Aliases
 alias ls 'ls -a --color=auto'
@@ -16,8 +18,12 @@ alias lg 'lazygit'
 
 # tmux autostart: nur wenn nicht schon in tmux und echtes Terminal
 if not set -q TMUX; and isatty stdout
-  #tmux attach; or tmux new-session
-  tmux;
+    # Bestehende Session anhängen, sonst neue erstellen
+    if tmux list-sessions &>/dev/null
+        exec tmux attach-session
+    else
+        exec tmux new-session
+    end
 end
 
 # Environment variables
